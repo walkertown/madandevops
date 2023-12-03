f3c7bbe81761e3fdee77dc583afcf08aa6ff1bf9
madan <madanponna@gmail.com>
2023-12-03 21:19:39 +0530

    test

--- src/components/PageViewer.vue
+++ src/components/PageViewer.vue
@@ -1,7 +1,32 @@
 <template>
-    <div class="container">
+    <!-- <div class="container">
         <h1>{{ page.pageTitle }}</h1>
         <p>{{ page.content }}</p>
+    </div> -->
+
+    <div class="container-fluid mt-3">
+        <div class="row">
+            <div class="col p-6 bg-primary text-white">
+                <span class="badge rounded-pill bg-primary" style="font-size: larger;">RAM IT <br /> DevOps
+                    Instructor</span>
+            </div>
+            <div class="col p-3 bg-primary text-white">
+                <span class="badge rounded-pill bg-primary  float-end" style="font-size: larger;">WhatsApp
+                    +9999988888</span>
+            </div>
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col p-3 bg-white text-white"></div>
+        <div class="col p-3 bg-success text-white rounded text-center">
+            <span class="badge rounded-pill bg-success " style="font-size: larger;">
+                DevOps New Batch <br />
+                December 9th (Thrusday) <br />
+                7:00 PM to 8:30 PM IST
+            </span>
+        </div>
+        <div class="col p-3 bg-white text-white"></div>
     </div>
 </template>
 
