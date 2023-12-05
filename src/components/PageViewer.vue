29f6d8d068213f7cf29fa4277fba4aae75cf2590
madan <madanponna@gmail.com>
2023-12-05 19:41:26 +0530

    test

--- src/components/PageViewer.vue
+++ src/components/PageViewer.vue
@@ -18,11 +18,11 @@
       </div>
     </div>
   </div>
-  
-  
+
+
   <div id="demo" class="carousel slide" data-bs-ride="carousel">
 
-  
+
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
       <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
@@ -32,7 +32,7 @@
       <button type="button" data-bs-target="#demo" data-bs-slide-to="5"></button>
     </div>
 
-  
+
     <div class="carousel-inner">
 
       <div class="carousel-item active">
@@ -42,7 +42,7 @@
           <p></p>
         </div>
       </div>
-      
+
       <div class="carousel-item">
         <img src="jenkins.jpg" class="d-block" style="width:100%">
         <div class="carousel-caption">
@@ -58,7 +58,7 @@
           <p></p>
         </div>
       </div>
-    
+
       <div class="carousel-item">
         <img src="terraform.jpg" class="d-block" style="width:100%">
         <div class="carousel-caption">
@@ -66,9 +66,9 @@
           <p></p>
         </div>
       </div>
-      
+
       <div class="carousel-item">
-        <img src="prometheus.jpg"  class="d-block" style="width:100%">
+        <img src="prometheus.jpg" class="d-block" style="width:100%">
         <div class="carousel-caption">
           <h3></h3>
           <p></p>
@@ -76,7 +76,7 @@
       </div>
 
       <div class="carousel-item">
-        <img src="grafana.jpg"  class="d-block" style="width:100%">
+        <img src="grafana.jpg" class="d-block" style="width:100%">
         <div class="carousel-caption">
           <h3></h3>
           <p></p>
@@ -84,15 +84,74 @@
       </div>
     </div>
 
-  
+
     <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
       <span class="carousel-control-prev-icon"></span>
     </button>
     <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
       <span class="carousel-control-next-icon"></span>
     </button>
-  </div>  
+  </div>
 
+  <div class="m-4 text-center">
+    <h4 class="ms-4">DevOps Syllabus</h4>
+
+    <div class="container d-flex mt-4 p-4">
+      <div class="card mb-3" style="max-width:540px;">
+      <div class="row g-0">
+        <div class="col-md-6">
+          <img src="devops.png" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-6">
+          <div class="card-body">
+            <h5 class="card-title">
+              INTRODUCTION TO DEVOPS
+            </h5>
+            <p class="card-text">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">What is Devops?</li>
+                  <li class="list-group-item">Why Devops?</li>
+                  <li class="list-group-item">Devops Tools</li>
+                  <li class="list-group-item">Differences between traditional methodologies and DEVOPS-approach</li>
+                  <li class="list-group-item">Big Picture of CI/CD Devops Delivery Pipeline</li>
+                </ul>
+            </p>            
+          </div>
+        </div>
+      </div>
+    </div>
+      <div class="card mb-3" style="max-width:540px;">
+        <div class="row g-0">
+          <div class="col-md-6">
+            <img src="linux.png"
+              class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-6">
+            <div class="card-body">
+              <h5 class="card-title">OPERATING SYSTEM & LINUX BASICS</h5>
+              <p class="card-text">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Introduction to Operating Systems</li>
+                  <li class="list-group-item">Introduction to Virtual Machines</li>
+                  <li class="list-group-item">Linux File Systems</li>
+                  <li class="list-group-item">Introduction to Command Line Interface</li>
+                  <li class="list-group-item">Basic Linux Commands</li>
+                  <li class="list-group-item">Package Manager - Installing Software in Linux</li>
+                  <li class="list-group-item">Vim Text Editor</li>
+                  <li class="list-group-item">Users and Permissions</li>
+                  <li class="list-group-item">Shell Scripting</li>
+                  <li class="list-group-item">Environment Variables</li>
+                  <li class="list-group-item">SSH -Secure Shell</li>
+                </ul>
+              </p>              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
 </template>
 
 <script>
